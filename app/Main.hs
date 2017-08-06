ecf5877a6db7456def79bc0f5362196cf1277927
Naoto Shimazaki <nshimaza@cisco.com>
2017-08-06 18:30:12 +0900

    add simple sample code to main

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,29 @@
 
 module Main where
 
-import           Network.HTTP.Simple
+import Conduit
+import           Data.ByteString.Char8 as BC8 (pack)
+import           Data.Semigroup        ((<>))
+import           Options.Applicative
 
 import           Network.CiscoSpark
 
 
+auth :: Parser Authorization
+auth = Authorization . BC8.pack <$> strOption
+    (  long "auth"
+    <> short 'a'
+    <> metavar "AUTHORIZATION"
+    <> help "Authorization token string passed via HTTP Authorization header.")
+
+opts = info (auth <**> helper)
+    (  fullDesc
+    <> progDesc "Sample porgram demonstrating how to use cisco-spark-api."
+    <> header "cisco-spark-api-exe -a auth"
+    )
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+    auth <- execParser opts
+    print auth
+    runConduit $ streamTeamList auth ciscoSparkBaseRequest .| mapM_C print
