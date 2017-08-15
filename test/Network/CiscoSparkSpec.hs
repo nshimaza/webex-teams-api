5fdf950af8362557a9da60faf50ee006a0481b41
Naoto Shimazaki <nshimaza@cisco.com>
2017-08-15 10:44:59 +0900

    reduce redundant lambdas.

--- test/Network/CiscoSparkSpec.hs
+++ test/Network/CiscoSparkSpec.hs
@@ -126,7 +126,7 @@ spec = do
             let req = setRequestPath "/v1/teams"
                     $ setRequestMethod "GET"
                     $ mockBaseRequest
-                testData = map (\tl -> encode $ TeamList tl) teamListList
+                testData = map (encode . TeamList) teamListList
 
             svr <- startMockServer $ \req respond -> do
                 putMVar receivedReqMVar req
@@ -249,7 +249,7 @@ spec = do
             stopMockServer svr
 
         it "streamPersonList streams Team with automatic pagination" $ do
-            svr <- startMockServer $ paginationApp $ map (\pl -> encode $ PersonList pl) personListList
+            svr <- startMockServer $ paginationApp $ map (encode . PersonList) personListList
 
             res <- runConduit $ streamPersonList dummyAuth mockBaseRequest def .| sinkList
             res `shouldBe` concat personListList
@@ -315,7 +315,7 @@ spec = do
             stopMockServer svr
 
         it "streamTeamList streams Team with automatic pagination" $ do
-            svr <- startMockServer $ paginationApp $ map (\tl -> encode $ TeamList tl) teamListList
+            svr <- startMockServer $ paginationApp $ map (encode . TeamList) teamListList
 
             res <- runConduit $ streamTeamList dummyAuth mockBaseRequest .| sinkList
             res `shouldBe` concat teamListList
@@ -420,7 +420,7 @@ spec = do
             stopMockServer svr
 
         it "streamTeamMembershipList streams TeamMembership with automatic pagination" $ do
-            svr <- startMockServer $ paginationApp $ map (\pl -> encode $ TeamMembershipList pl) teamMembershipListList
+            svr <- startMockServer $ paginationApp $ map (encode . TeamMembershipList) teamMembershipListList
 
             res <- runConduit $ streamTeamMembershipList dummyAuth mockBaseRequest def .| sinkList
             res `shouldBe` concat teamMembershipListList
@@ -532,7 +532,7 @@ spec = do
             stopMockServer svr
 
         it "streamRoomList streams Room with automatic pagination" $ do
-            svr <- startMockServer $ paginationApp $ map (\rl -> encode $ RoomList rl) roomListList
+            svr <- startMockServer $ paginationApp $ map (encode . RoomList) roomListList
 
             res <- runConduit $ streamRoomList dummyAuth mockBaseRequest def .| sinkList
             res `shouldBe` concat roomListList
@@ -644,7 +644,7 @@ spec = do
             stopMockServer svr
 
         it "streamMembershipList streams Membership with automatic pagination" $ do
-            svr <- startMockServer $ paginationApp $ map (\ml -> encode $ MembershipList ml) membershipListList
+            svr <- startMockServer $ paginationApp $ map (encode . MembershipList) membershipListList
 
             res <- runConduit $ streamMembershipList dummyAuth mockBaseRequest def .| sinkList
             res `shouldBe` concat membershipListList
@@ -719,7 +719,7 @@ spec = do
             stopMockServer svr
 
         it "streamOrganizationList streams Organization with automatic pagination" $ do
-            svr <- startMockServer $ paginationApp $ map (\ol -> encode $ OrganizationList ol) organizationListList
+            svr <- startMockServer $ paginationApp $ map (encode . OrganizationList) organizationListList
 
             res <- runConduit $ streamOrganizationList dummyAuth mockBaseRequest .| sinkList
             res `shouldBe` concat organizationListList
@@ -812,7 +812,7 @@ spec = do
             stopMockServer svr
 
         it "streamLicenseList streams License with automatic pagination" $ do
-            svr <- startMockServer $ paginationApp $ map (\ll -> encode $ LicenseList ll) licenseListList
+            svr <- startMockServer $ paginationApp $ map (encode . LicenseList) licenseListList
 
             res <- runConduit $ streamLicenseList dummyAuth mockBaseRequest def .| sinkList
             res `shouldBe` concat licenseListList
@@ -880,7 +880,7 @@ spec = do
             stopMockServer svr
 
         it "streamRoleList streams Role with automatic pagination" $ do
-            svr <- startMockServer $ paginationApp $ map (\rl -> encode $ RoleList rl) roleListList
+            svr <- startMockServer $ paginationApp $ map (encode . RoleList) roleListList
 
             res <- runConduit $ streamRoleList dummyAuth mockBaseRequest .| sinkList
             res `shouldBe` concat roleListList
