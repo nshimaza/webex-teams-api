583c9234e2146617a43e8ede1aca144227ae7dd6
Naoto Shimazaki <nshimaza@cisco.com>
2017-08-28 19:19:17 +0900

    add membership list sample code.

--- app/Main.hs
+++ app/Main.hs
@@ -17,9 +17,10 @@ import           Network.CiscoSpark
 data Command
     = TeamListCommand Int
     | RoomListCommand Int RoomFilter
-    | PeopleListCommand Int PersonFilter
+    | PersonListCommand Int PersonFilter
     | PersonDetailCommand PersonId
     | RoomDetailCommand RoomId
+    | MembershipListCommand Int MembershipFilter
     | MembershipDetailCommand MembershipId
     | MessageDetailCommand MessageId
     | TeamDetailCommand TeamId
@@ -35,13 +36,6 @@ data Options = Options
 {-
     Common command line option parsers
 -}
--- authParser :: Parser Authorization
--- authParser = Authorization . BC8.pack <$> strOption
---     (  long    "auth"
---     <> short   'a'
---     <> metavar "AUTHORIZATION"
---     <> help    "Authorization token string passed via HTTP Authorization header")
-
 countParser :: Parser Int
 countParser = option auto
     (  long    "count"
@@ -51,6 +45,15 @@ countParser = option auto
     <> help "Maximum number of items to print"
     )
 
+emailOptParser :: Parser Email
+emailOptParser = Email . T.pack <$> strOption
+    (  long     "email"
+    <> short    'e'
+    <> metavar  "EMAIL_ADDRESS"
+    <> help     "An email address to filter result"
+    )
+
+
 {-
     Person specific command line option parsers
 -}
@@ -58,14 +61,6 @@ personIdParser :: Parser PersonId
 personIdParser = PersonId . T.pack <$> strArgument
     (  metavar "PERSON_ID"
     <> help    "Identifier of a person"
-    )
-
-emailParser :: Parser Email
-emailParser = Email . T.pack <$> strOption
-    (  long     "email"
-    <> short    'e'
-    <> metavar  "EMAIL_ADDRESS"
-    <> help     "An email address of the person to look up"
     )
 
 displayNameParser :: Parser DisplayName
@@ -85,10 +80,10 @@ organizationIdOptParser = OrganizationId . T.pack <$> strOption
     )
 
 personFilterParser :: Parser PersonFilter
-personFilterParser = PersonFilter <$> optional emailParser <*> optional displayNameParser <*> optional organizationIdOptParser
+personFilterParser = PersonFilter <$> optional emailOptParser <*> optional displayNameParser <*> optional organizationIdOptParser
 
-peopleListOptParser :: Parser Command
-peopleListOptParser = PeopleListCommand <$> countParser <*> personFilterParser
+personListOptParser :: Parser Command
+personListOptParser = PersonListCommand <$> countParser <*> personFilterParser
 
 personDetailOptParser :: Parser Command
 personDetailOptParser = PersonDetailCommand <$> personIdParser
@@ -142,6 +137,28 @@ membershipIdParser = MembershipId . T.pack <$> strArgument
     <> help    "Identifier of a membership"
     )
 
+roomIdOptParser :: Parser RoomId
+roomIdOptParser = RoomId . T.pack <$> strOption
+    (  long     "room"
+    <> short    'r'
+    <> metavar "ROOM_ID"
+    <> help    "Identifier of a room to be searched"
+    )
+
+personIdOptParser :: Parser PersonId
+personIdOptParser = PersonId . T.pack <$> strOption
+    (  long     "person"
+    <> short    'p'
+    <> metavar "PERSON_ID"
+    <> help    "Identifier of a person to filter result"
+    )
+
+membershipListFilterParser :: Parser MembershipFilter
+membershipListFilterParser = MembershipFilter <$> optional roomIdOptParser <*> optional personIdOptParser <*> optional emailOptParser
+
+membershipListOptParser :: Parser Command
+membershipListOptParser = MembershipListCommand <$> countParser <*> membershipListFilterParser
+
 membershipDetailOptParser :: Parser Command
 membershipDetailOptParser = MembershipDetailCommand <$> membershipIdParser
 
@@ -194,10 +211,11 @@ commandSubParser :: Parser Command
 commandSubParser = hsubparser
     (  command "team-list" (info teamListOptParser (progDesc "List belonging teams"))
     <> command "team-detail" (info teamDetailOptParser (progDesc "Get detail for a team by ID"))
-    <> command "person-list" (info peopleListOptParser (progDesc "List poeple"))
+    <> command "person-list" (info personListOptParser (progDesc "List people"))
     <> command "person-detail" (info personDetailOptParser (progDesc "Get detail for a person by ID"))
     <> command "room-list" (info roomListOptParser (progDesc "List belonging spaces"))
     <> command "room-detail" (info roomDetailOptParser (progDesc "Get detail for a team by ID"))
+    <> command "membership-list" (info membershipListOptParser (progDesc "List memberships of authenticated user"))
     <> command "membership-detail" (info membershipDetailOptParser (progDesc "Get detail for a membership by ID"))
     <> command "message-detail" (info messageDetailOptParser (progDesc "Get detail for a message by ID"))
     <> command "team-membership-detail" (info teamMembershipDetailOptParser (progDesc "Get detail for a team membership by ID"))
@@ -211,10 +229,13 @@ programOptions = info (commandSubParser <**> helper)
     )
 
 run :: Authorization -> Command -> IO ()
-run auth (PeopleListCommand count filter) =
+run auth (PersonListCommand count filter) =
     runConduit $ streamEntityWithFilter auth def filter .| takeC count .| mapM_C print
 
 run auth (RoomListCommand count filter) =
+    runConduit $ streamEntityWithFilter auth def filter .| takeC count .| mapM_C print
+
+run auth (MembershipListCommand count filter) =
     runConduit $ streamEntityWithFilter auth def filter .| takeC count .| mapM_C print
 
 run auth (TeamListCommand count) =
@@ -243,7 +264,7 @@ main :: IO ()
 main = do
     lookupEnv "SPARK_AUTH" >>= runIfEnvFound
       where
-        runIfEnvFound Nothing   = hPutStrLn stderr "Missing SPARK_AUTH.  Set Spark authentication to SPARK_AUTH environment variable."
+        runIfEnvFound Nothing   = hPutStrLn stderr "Missing SPARK_AUTH.  Set Spark authorization string to SPARK_AUTH environment variable."
         runIfEnvFound (Just s)  = do
             opts <- execParser programOptions
             print opts
